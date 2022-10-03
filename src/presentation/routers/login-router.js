40b7f04aa169bc31bb6c754aa247393b93e25e26
ifoxxiedev <obede.drrt.info@gmail.com>
2022-10-03 14:21:27 -0300

    test: ensure LoginRouter calls AuthUseCase with correct params

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -2,6 +2,10 @@ import { MissingParamError } from '../errors/missing-param-error'
 import { HttpResponse } from '../helpers/http-response'
 
 export class LoginRouter {
+  constructor (authUseCase) {
+    this.authUseCase = authUseCase
+  }
+
   route (httpRequest) {
     if (!httpRequest || !httpRequest.payload) {
       return HttpResponse.serverError()
@@ -16,5 +20,7 @@ export class LoginRouter {
     if (!password) {
       return HttpResponse.badRequest(new MissingParamError('password'))
     }
+
+    this.authUseCase.auth(email, password)
   }
 }
--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,5 +1,21 @@
 import { MissingParamError } from '../errors/missing-param-error'
 import { LoginRouter } from './login-router'
+class AuthUseCaseSpy {
+  auth (email, password) {
+    this.email = email
+    this.password = password
+  }
+}
+
+const makeSut = () => {
+  const authUseCaseSpy = new AuthUseCaseSpy()
+  const loginRouter = new LoginRouter(authUseCaseSpy)
+
+  return {
+    loginRouter,
+    authUseCaseSpy
+  }
+}
 
 describe('Login Router', () => {
   it('Should return 400 if no email is provided', () => {
@@ -9,7 +25,7 @@ describe('Login Router', () => {
       }
     }
 
-    const loginRouter = new LoginRouter()
+    const { loginRouter } = makeSut()
     const httpResponse = loginRouter.route(httpRequest)
 
     expect(httpResponse.statusCode).toBe(400)
@@ -23,7 +39,7 @@ describe('Login Router', () => {
       }
     }
 
-    const loginRouter = new LoginRouter()
+    const { loginRouter } = makeSut()
     const httpResponse = loginRouter.route(httpRequest)
 
     expect(httpResponse.statusCode).toBe(400)
@@ -31,14 +47,32 @@ describe('Login Router', () => {
   })
 
   it('Should return 500 if  no httpRequest is provided', () => {
-    const loginRouter = new LoginRouter()
+    const { loginRouter } = makeSut()
     const httpResponse = loginRouter.route()
     expect(httpResponse.statusCode).toBe(500)
   })
 
   it('Should return 500 if no httpRequest no has payload', () => {
-    const loginRouter = new LoginRouter()
+    const { loginRouter } = makeSut()
     const httpResponse = loginRouter.route({})
     expect(httpResponse.statusCode).toBe(500)
+  })
+
+  it.only('Should call AuthUseCase with correct params', () => {
+    const { loginRouter, authUseCaseSpy } = makeSut()
+
+    const httpRequest = {
+      payload: {
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      }
+    }
+
+    //  SpyOn
+    // jest.spyOn(authUseCase, 'auth').mockResolvedValue(httpRequest.payload.email)
+    loginRouter.route(httpRequest)
+
+    expect(authUseCaseSpy.email).toBe(httpRequest.payload.email)
+    expect(authUseCaseSpy.password).toBe(httpRequest.payload.password)
   })
 })
