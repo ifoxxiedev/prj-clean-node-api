940afacc10587e22c50d6f808ccd80ed85712a88
ifoxxiedev <obede.drrt.info@gmail.com>
2022-10-03 13:24:09 -0300

    ensure LoginRouter returns 500 if an httpRequest is provided

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,5 +1,11 @@
 class LoginRouter {
   route (httpRequest) {
+    if (!httpRequest || !httpRequest.payload) {
+      return {
+        statusCode: 500
+      }
+    }
+
     const { email, password } = httpRequest.payload
 
     if (!email || !password) {
@@ -35,5 +41,17 @@ describe('Login Router', () => {
     const httpResponse = loginRouter.route(httpRequest)
 
     expect(httpResponse.statusCode).toBe(400)
+  })
+
+  it('Should return 500 if  no httpRequest is provided', () => {
+    const loginRouter = new LoginRouter()
+    const httpResponse = loginRouter.route()
+    expect(httpResponse.statusCode).toBe(500)
+  })
+
+  it('Should return 500 if no httpRequest no has payload', () => {
+    const loginRouter = new LoginRouter()
+    const httpResponse = loginRouter.route({})
+    expect(httpResponse.statusCode).toBe(500)
   })
 })
